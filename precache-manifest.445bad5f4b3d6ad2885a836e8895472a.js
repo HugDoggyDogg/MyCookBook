26f57340b766f4dfab180e0de68a3f2042e30c4a
Jonathan Lecointe <jonathan@energic.io>
2020-05-02 14:16:21 +0200

    Updates

---
+++ precache-manifest.445bad5f4b3d6ad2885a836e8895472a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "22b30bc5a46dfc06f21a",
+    "url": "/MyCookBook/static/js/app.f9153dd6.chunk.js"
+  },
+  {
+    "revision": "dc0bde5a9006ab8c1cc2",
+    "url": "/MyCookBook/static/js/runtime~app.add0a17d.js"
+  },
+  {
+    "revision": "b15d53ee85914a2c0438",
+    "url": "/MyCookBook/static/js/2.88dccbd5.chunk.js"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/MyCookBook/serve.json"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/MyCookBook/expo-service-worker.js"
+  },
+  {
+    "revision": "afd5733ac789ab1795f1ed7a26e1ecd3",
+    "url": "/MyCookBook/favicon-16.png"
+  },
+  {
+    "revision": "3d85359b37ca7590f8e91d593d02ddaf",
+    "url": "/MyCookBook/favicon-32.png"
+  },
+  {
+    "revision": "5e52768b0f7cbb62cbe07751add33101",
+    "url": "/MyCookBook/favicon.ico"
+  },
+  {
+    "revision": "e8d94dec22cda86dbfa1bc392619cb7e",
+    "url": "/MyCookBook/index.html"
+  },
+  {
+    "revision": "f973b047dd06e3b3b94e7b0f25a16af9",
+    "url": "/MyCookBook/manifest.json"
+  },
+  {
+    "revision": "5cf9cd0a75817e31e71e222fb9aec283",
+    "url": "/MyCookBook/register-service-worker.js"
+  }
+];
